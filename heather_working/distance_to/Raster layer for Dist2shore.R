f8f2773786b45877409d2b22d137eaf2157e20a4
Heather Welch <heather.welch@noaa.gov>
2021-03-15 11:22:29 -0700

    changing app and calculating distance to

---
+++ heather_working/distance_to/Raster layer for Dist2shore.R
@@ -0,0 +1,65 @@
+## megan's code to create empty raster
+source("/Users/heatherwelch/Dropbox/Gulls/heather_working/github_Gulls_gone_wild/general_functions/load_libraries.R")
+library(raster)
+
+x <- raster()
+x2<-crop(x,  extent( -125, -121,36 ,40))
+nrow(x2) <- 444
+ncol(x2) <- 361
+#values(x2) <-1:160284
+
+r <- setValues(x2, sample(x = 0:500, size = ncell(x2), replace = T))
+projected_raster <- projectRaster(r, crs = "+proj=utm +zone=10 +datum=WGS84 +units=m ")
+res(projected_raster ) <- 1000 #1 km resolution 
+
+r2 <- setValues(projected_raster, sample(x = 0:500, size = ncell(projected_raster), replace = T))
+
+raster_back <- projectRaster(r2, crs = "+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0")
+
+plot(raster_back)
+map(add=T)
+writeRaster(raster_back,"/Users/heatherwelch/Dropbox/Gulls/static_rasters/studyarea.grd")
+#writeRaster()
+
+## distance to shore ####
+# need to split
+cclme=st_read("/Users/heatherwelch/Dropbox/OLE/spatial_data/lme/lme.shp") 
+studyarea=st_read("/Users/heatherwelch/Dropbox/Gulls/shapefiles/us_medium_shoreline/us_medium_shoreline.shp")
+sa=as_Spatial(studyarea)
+
+sa_offshore=mask(raster_back,cclme)
+sa_inshore=mask(raster_back,cclme,inverse=T)
+# test=merge(sa_offshore,sa_inshore) ## great it works!
+test_inshore=raster::distance(sa_offshore)
+test_inshore2=mask(test_inshore,cclme,inverse=T)*-1
+test_offshore=raster::distance(sa_inshore)
+test_offshore2=mask(test_offshore,cclme)
+master=merge(test_offshore2,test_inshore2)
+writeRaster(master,"/Users/heatherwelch/Dropbox/Gulls/static_rasters/dist_shore.grd")
+
+## distance to dump ####
+trash=read.csv("/Users/heatherwelch/Dropbox/Gulls/shapefiles/lmopdataca_reduced.csv") %>% 
+  dplyr::select(Latitude,Longitude)
+dat= data.frame(thing=c("Pier 96 recology","recology sunset","oakland airport"),
+                Latitude=c(37.741734,37.709994,37.713622), 
+                Longitude=c(-122.371846,-122.390314,-122.193076), 
+                stringsAsFactors=FALSE) %>% dplyr::select(Latitude,Longitude) 
+
+masterTrash=rbind(trash,dat)%>% .[complete.cases(.),]
+coordinates(masterTrash)=~Longitude+Latitude
+
+test=raster::distanceFromPoints(raster_back,masterTrash)
+writeRaster(test,"/Users/heatherwelch/Dropbox/Gulls/static_rasters/dist_dump.grd")
+
+## fast food? ####
+## accessed from: https://data.world/datafiniti/fast-food-restaurants-across-america
+## Datafiniti_Fast_Food_Restaurants_Jun19.csv
+ff=read.csv("/Users/heatherwelch/Downloads/Datafiniti_Fast_Food_Restaurants_Jun19.csv") %>% 
+  filter(longitude>(-125) & longitude<(-121)) %>% filter(latitude>(36) & latitude<(40))
+
+ff_2=dplyr::select(ff,c(longitude,latitude)) 
+coordinates(ff_2)=~longitude+latitude
+test_ff=raster::distanceFromPoints(raster_back,ff_2)
+writeRaster(test_ff,"/Users/heatherwelch/Dropbox/Gulls/static_rasters/dist_fastfood.grd")
+
+
--- heather_working/gulls_gone_wild_app/app.R
+++ heather_working/gulls_gone_wild_app/app.R
@@ -12,37 +12,20 @@ library(glue)
 library(shinydashboard)
 library(leaflet)
 library(tidyverse)
+# library(viridis)
 
 # studyarea=st_read("data/us_medium_shoreline/us_medium_shoreline.shp")
 # bridges=st_read("data/bay_area_bridges/bayarea_bridges.shp")
 trash=read.csv("data/lmopdataca_reduced.csv")
-gulls=read.csv("data/gulls.csv")
+gulls=read.csv("data/gulls.csv") %>% mutate(day=yday(date)) %>% mutate(date=as.Date(date))
 gull_ids=unique(gulls$birdID)
+whales=read.csv("data/OBIS_whales.csv") %>% mutate(date=as.Date(date))
+
 
 megan_points= data.frame(thing=c("Pier 96 recology","recology sunset","oakland airport","SouthFI"),
                 lat=c(37.741734,37.709994,37.713622,37.698321), 
                 lon=c(-122.371846,-122.390314,-122.193076,-123.000313), 
                 stringsAsFactors=FALSE) 
-# #WP=readShapeSpatial("data/shp/WesternApproachSeparationZone.shp")
-# projstring <- '+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0'
-# WP=readOGR("data/shp/WesternPolygon.shp")
-# WP <-spTransform(WP, CRS("+proj=longlat +datum=WGS84")) #convert from UTM to LatLon
-# #ship_lane=readShapeSpatial("data/shp/WesternApproachShippingLanes.shp")
-# #VSR_zone=readShapeSpatial("data/shp/WesternVSRZone.shp")
-# TSS=readShapeSpatial("data/shp/TSSpolygon.shp")
-# proj4string(TSS)=projstring
-# #WP=readShapeSpatial("data/shp/WesternPolygon.shp")
-# r2016="data/2016"
-# r2009="data/2009"
-# r2015="data/2015"
-
-# times=seq(as.Date("2016/01/01"),as.Date("2016/12/31"),by="day") %>% lapply(.,function(x)gsub("2016-","",x)) %>% unlist() %>% 
-#   lapply(.,function(x)gsub("-",".",x)) %>% unlist() %>% as.numeric()
-
-#times=seq(as.Date("2016/01/01"),as.Date("2016/12/31"),by="day") %>% lapply(.,function(x)gsub("2016-","",x)) %>% unlist() #%>% 
-# 
-# months=seq(1,12,by=1)%>% str_pad(.,2,pad="0")
-# days=seq(1,31,by=1)%>% str_pad(.,2,pad="0")
 
 
 ui <- dashboardPage(skin="blue",
@@ -54,15 +37,29 @@ ui <- dashboardPage(skin="blue",
                       width = 300,
                       sidebarMenu(id = 'sidebarmenu',
                                   #div(style="text-align:center",tags$div(img(src='blwh_icon3.png',width="40%"))),
-                                  menuItem("Select individual", tabName='date',icon=icon("clock-o",lib='font-awesome')),
+                                  menuItem("View by individual", tabName='date',icon=icon("clock-o",lib='font-awesome')),
+                                  conditionalPanel("input.sidebarmenu ==='date'",
                                     # column(selectInput("months","Month",months),width = 6),
                                     # column(selectInput("days","Day",days),width = 6)),
-                                    selectInput("id", label = 'Bird ID',gull_ids))),
+                                    selectInput("id", label = 'Bird ID',gull_ids)),
+                      menuItem("View by date + whales", tabName='date2',icon=icon("clock-o",lib='font-awesome')),
+                      conditionalPanel("input.sidebarmenu ==='date2'",
+                                    sliderInput("dateSel", "Date",min=min(gulls$date),max=max(gulls$date),value=min(gulls$date),animate = animationOptions(interval = 500, loop = T))
+                                  ))),
                     
                     dashboardBody(
-                      fluidRow(
+                      
+                        tabItems(
+                          tabItem(tabName = "date",
+                                  fluidRow(
                         column(h4(htmlOutput("dt2")),width=12),
-                        column(h4(div(style="text-align:center",tags$b("2009"))),width=12,leafletOutput("map09"))
+                        column(h4(div(style="text-align:center",tags$b("Data"))),width=12,leafletOutput("map09"))
+                          )),
+                        tabItem(tabName = "date2",
+                                fluidRow(
+                                  column(h4(htmlOutput("dt3")),width=12),
+                                  column(h4(div(style="text-align:center",tags$b("Data"))),width=12,leafletOutput("map10"))
+                                ))
                         
                       )
                     ))
@@ -72,13 +69,19 @@ ui <- dashboardPage(skin="blue",
 server <- shinyServer(function(input, output) {
   
   bight_ext<-list()
-  bight_ext$lon<--122.5
-  bight_ext$lat<-37.35
-  bight_ext$zoom<-7
+  bight_ext$lon<--122.8
+  bight_ext$lat<-37.75
+  bight_ext$zoom<-10
   
   output$dt2=renderText({
     display=input$id
     paste("<b>Bird ID is ",display,"</b>")
+    #HTML(paste(a))
+  })
+  
+  output$dt3=renderText({
+    display=input$dateSel
+    paste("<b>Date shown is ",display,"</b>")
     #HTML(paste(a))
   })
   
@@ -112,8 +115,106 @@ server <- shinyServer(function(input, output) {
     
   })
   
-  
+  gettingThegulls=reactive({
+    req(input$dateSel)
+  gulls %>% dplyr::filter(date==input$dateSel)
   })
+  
+  gettingThewhales=reactive({
+    req(input$dateSel)
+    whales %>% dplyr::filter(date==input$dateSel)
+  })
+  
+  output$map10 <- renderLeaflet({
+    
+    
+    # 
+    # bird=as.integer(input$dateSel)
+    # dat=gulls %>% dplyr::filter(date==bird) 
+
+    
+    
+    # pal <- colorFactor(
+    #   palette = 'Spectral',
+    #   domain = as.character(dat$date)
+    # )
+    
+    # lmap <- leaflet()
+    # lmap <- setView(lmap, bight_ext$lon, bight_ext$lat, zoom=bight_ext$zoom)
+    # lmap <- addProviderTiles(lmap, "Esri.WorldStreetMap")
+    # lmap <-addCircleMarkers(lmap,data=megan_points,lat=megan_points$lat,lng=megan_points$lon,radius=3,color = "pink",opacity=1)
+    # # lmap <-addCircleMarkers(lmap,data=dat,lat=dat$Latitude,lng=dat$Longitude,radius=1,color = ~pal(date))
+    # lmap <-addCircleMarkers(lmap,data=trash,lat=trash$Latitude,lng=trash$Longitude,radius=1,color = "black")
+    
+    leaflet() %>% 
+    setView(bight_ext$lon, bight_ext$lat, zoom=bight_ext$zoom) %>% 
+    addProviderTiles( "Esri.WorldStreetMap") %>% 
+    addCircleMarkers(data=megan_points,lat=megan_points$lat,lng=megan_points$lon,radius=3,color = "pink",opacity=1) %>% 
+    # lmap <-addCircleMarkers(lmap,data=dat,lat=dat$Latitude,lng=dat$Longitude,radius=1,color = ~pal(date))
+    addCircleMarkers(data=trash,lat=trash$Latitude,lng=trash$Longitude,radius=1,color = "black") 
+    
+    
+   
+    # lmap <- addLegend(lmap, "topright", values = unique(as.character(dat$date)),pal=pal,title="Date")
+    # lmap
+    
+    
+    
+  })
+  
+  observe({
+
+    pal <- colorFactor(
+      palette = 'Spectral',
+      domain = as.character(gettingThegulls()$birdID)
+    )
+
+    leafletProxy("map10",data=gettingThegulls()) %>%
+      clearGroup(group="gulls") %>%
+      addCircleMarkers(group="gulls",lat=~Latitude,lng=~Longitude,radius=1,color = ~pal(birdID))
+  })
+  
+  observe({
+
+    ## retained stuff for adding color
+    # pal <- colorFactor(
+    #   palette = colorRampPalette(rainbow(10))(length(gettingThewhales()$common)), 
+    #   domain = gettingThewhales()$common)
+    # 
+    # leafletProxy("map10",data=gettingThewhales()) %>%
+    #   clearGroup(group="whales") %>%
+    #   addCircleMarkers(group="whales",lat=~latitude,lng=~longitude,radius=2,color=~pal(common))
+    
+    leafletProxy("map10",data=gettingThewhales()) %>%
+      clearGroup(group="whales") %>%
+      addMarkers(group="whales",lat=~latitude,lng=~longitude, popup = ~as.character(common))
+                      
+  })
+  
+
+observe({
+
+  pal <- colorFactor(
+    palette = 'Spectral',
+    domain = as.character(gettingThegulls()$birdID)
+  )
+
+  leafletProxy("map10",data=gettingThegulls()) %>%
+    clearControls() %>%
+    addLegend("topright", values = ~birdID,pal=pal,title="Bird ID")
+})
+
+})
 
 
 shinyApp(ui = ui, server = server)
+
+# data(quakes)
+# 
+# pal <- colorFactor(
+#   palette = colorRampPalette(rainbow(2)),
+#   domain = whales$common)
+# 
+# # Show first 20 rows from the `quakes` dataset
+# leaflet(data = whales[1:20,]) %>% addTiles() %>%
+#   addMarkers(~longitude, ~latitude, popup = ~as.character(common), label = ~as.character(common))
